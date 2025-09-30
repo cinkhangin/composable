7af5a3d05ffb78ed8158293b41011b90009dad68
Naulian <cinkhangin@gmail.com>
2025-09-30 20:20:28 +0800

    Fix Counter file names

--- acc/src/main/java/com/naulian/composable/acc/counter/Counter.kt
+++ acc/src/main/java/com/naulian/composable/acc/counter/Counter.kt
@@ -1,90 +1,17 @@
 package com.naulian.composable.acc.counter
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.naulian.composable.core.LocalNavController
-import com.naulian.composable.core.component.CodeBlock
-import com.naulian.composable.core.component.ComposableTopAppBar
-import kotlinx.coroutines.delay
 
-@Composable
-fun CounterScreen() {
-    val navController = LocalNavController.current
-
-    CounterScreenUI (
-        onBack = { navController.navigateUp() }
-    )
-}
-
-@Composable
-fun CounterScreenUI(onBack: () -> Unit = {}) {
-    val code = remember { animatedCounterCode }
-    var count by remember { mutableStateOf(0) }
-    LaunchedEffect(Unit) {
-        while (true) {
-            delay(1000)
-            count++
-        }
-    }
-
-    Scaffold(
-        topBar = {
-            ComposableTopAppBar(
-                title = "Animated Counter",
-                onBack = onBack
-            )
-        }
-    ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(40.dp)
-        ) {
-            item {
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    AnimatedCounter(count = count, modifier = Modifier.fillMaxWidth())
-
-                    Spacer(Modifier.height(16.dp))
-
-                }
-            }
-
-            item {
-                CodeBlock(
-                    source = code,
-                    language = "kotlin",
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-        }
-    }
-}
-
-val animatedCounterCode = """
 @Composable
 fun AnimatedCounter(
     count: Int,
@@ -92,7 +19,11 @@ fun AnimatedCounter(
 ) {
     val animatedCount by animateIntAsState(
         targetValue = count,
-        label = "AnimatedCounter"
+        label = "AnimatedCounter",
+        animationSpec = tween(
+            durationMillis = count * 1000,
+            easing = LinearEasing
+        )
     )
 
     Box(
@@ -105,4 +36,3 @@ fun AnimatedCounter(
         )
     }
 }
-""".trimIndent()
