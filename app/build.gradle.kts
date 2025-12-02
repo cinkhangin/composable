fcd0344ae9c0d6813ca14a4db5002fe8163d7dcb
Naulian <cinkhangin@gmail.com>
2025-12-02 14:11:33 +0800

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = packageName
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
