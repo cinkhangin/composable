d185b72060b977b53ecccefb7540b477030b2b51
Naulian <cinkhangin@gmail.com>
2025-12-03 15:11:06 +0800

    Delete unused modules

---
+++ app/src/main/java/com/naulian/composable/Research.kt
@@ -0,0 +1,10 @@
+package com.naulian.composable
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Research(modifier: Modifier = Modifier) {
+    Text(modifier = modifier, text = "hello")
+}
